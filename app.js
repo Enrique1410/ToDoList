268bb278259682150461c32e57aa61a149a25d0a
Enrique Aspera <enrique.alejandro.aspera@gmail.com>
2022-12-12 09:33:43 +0000

    Added additional pages and refactor code in modules

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = [];
+const workItems = [];
 
 app.set('view engine', "ejs");
 
@@ -12,18 +14,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    let today = new Date();
-    let options = { weekday: "long", day: "numeric", month: "long" };
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render('list', { kindOfDay: day, newListItems: items });
+    const day = date.getDate();
+    res.render('list', { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    items.push(item);
 
-    res.redirect("/")
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    } else {
+        items.push(item);
+        res.redirect("/")
+    }
+
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
 });
 
 app.listen(3000, () => {
